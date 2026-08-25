373786a285f0a7b9f5f15e9cf4870d10b31a33ee
Rishi <rishi_j@icloud.com>
2026-08-25 19:14:48 +0100

    Initial Commit

---
+++ tictactoe/tictactoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tictactoeApp.swift
+//  tictactoe
+//
+//  Created by Rishi Jansari on 25/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct tictactoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
